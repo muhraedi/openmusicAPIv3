24ea6b97cdd2eb1c40ffd645088eb2caa2a25e40
muhraedi <muhraedi@gmail.com>
2023-04-16 23:28:34 +0800

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase */
+const mapDBToModel = ({
+  id,
+  name,
+  year,
+  title,
+  genre,
+  performer,
+  duration,
+  album_id,
+}) => ({
+  id,
+  name,
+  year,
+  title,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = mapDBToModel;
